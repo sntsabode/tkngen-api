b91a96655b82346839ba2f5a4a266a1341140d1e
snts.abode <snts.abode@gmail.com>
2021-07-28 18:48:32 +0200

    contract deployment module

--- lib/lib/deploy.ts
+++ lib/lib/deploy.ts
@@ -1,21 +1,51 @@
-import { ISolcOutputContracts } from './compile'
+import { web3 } from '../web3'
+import { ISolcExtract } from './compile'
+import { TransactionReceipt } from 'web3-eth'
+import { untyped } from '../utils'
 
-export function makeRawTransaction(
-  coutputs: ISolcOutputContracts,
+export interface IDeployedContract {
+  ABI: untyped
+  addresses: string
+}
+
+export async function deploy(
   fromAccount: string,
-  gas: number
-): {
-  from: string
-  data: string
-  gas:  number
-} {
-  const contractFile = Object.keys(coutputs)[0]
-  const contractName = Object.keys(coutputs[contractFile])[0]
-  const contractCode = coutputs[contractFile][contractName].evm.bytecode.object
-  
-  return {
+  privateKey: string,
+  extract: ISolcExtract
+): Promise<TransactionReceipt> {
+  const gas = 2000000
+
+  const rawTX = {
     from: fromAccount,
-    data: contractCode,
+    data: extract.evmBytecode,
     gas
   }
+  return signAndSendTransaction(rawTX, privateKey)
+}
+
+export async function signAndSendTransaction(
+  transaction: IRawTransaction,
+  privateKey: string
+): Promise<TransactionReceipt> {
+  const signedTransaction = await web3.eth.accounts.signTransaction(
+    transaction, privateKey
+  ).catch(e => { throw e })
+
+  if (!signedTransaction.rawTransaction)
+    throw new Error('TX failed to sign')
+
+  const pendingTransaction = web3.eth.sendSignedTransaction(
+    signedTransaction.rawTransaction
+  )
+
+  return new Promise((resolve, reject) => {
+    pendingTransaction.on('error', err => reject(err))
+    pendingTransaction.on('receipt', receipt => resolve(receipt))
+  })
+}
+
+export interface IRawTransaction {
+  from: string
+  data: string
+  gas: string | number
 }
