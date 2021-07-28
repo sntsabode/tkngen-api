5fbf421388ec16392ea869b26c77d86695d27713
snts.abode <snts.abode@gmail.com>
2021-07-28 18:48:50 +0200

    `deploy` module test suite

--- tests/lib/deploy.test.ts
+++ tests/lib/deploy.test.ts
@@ -1,6 +1,10 @@
 import * as DeployMod from '../../lib/lib/deploy'
 import * as CompileMod from '../../lib/lib/compile'
 import { assert, expect } from 'chai'
+//import { assert, expect } from 'chai'
+
+const privateKey = '0xb864a8fb7cd9675b905b8c7ad831a649ef3759d0a88ae3f253abf51872fceb44'
+const account = '0xb8a859e749e843c45ec1b755a5221596be7dd6e9'
 
 const contract = `
 pragma solidity ^0.8.6;
@@ -10,27 +14,49 @@ contract Test {
 }
 `.trim()
 
-const contractByteCode = `608060405234801561001057600080fd5b5060b68061001f6000396000f3fe6080604052348015600f57600080fd5b506004361060285760003560e01c80630c55699c14602d575b600080fd5b60336047565b604051603e9190605d565b60405180910390f35b60006001905090565b6057816076565b82525050565b6000602082019050607060008301846050565b92915050565b600081905091905056fea264697066735822122023204e3fdcbae9a56db2c2a209c56d5511c0b39600a1e2461f97843f1ee2d1de64736f6c63430008060033`
-
 describe(
 'deploy module test suite',
 () => {
   it(
-  'Should call the makeRawTransaction function',
+  'Should call the signAndSendTransaction function',
+  async () => {
+    const gas = 2000000
+    const inputs = CompileMod.constructSolcInputs('Test.sol', contract)
+    const output = CompileMod.compile(inputs).contracts
+    const solcExtract = CompileMod.extractFromSolcOutput(output)
+
+    const receipt = await DeployMod.signAndSendTransaction({
+      from: account,
+      data: solcExtract.evmBytecode,
+      gas
+    }, privateKey)
+    
+    expect(receipt).to.have.property('transactionHash')
+    expect(receipt).to.have.property('transactionIndex')
+    expect(receipt).to.have.property('blockHash')
+    expect(receipt).to.have.property('blockNumber')
+    expect(receipt).to.have.property('from')
+    expect(receipt).to.have.property('to')
+    expect(receipt).to.have.property('gasUsed')
+    expect(receipt).to.have.property('cumulativeGasUsed')
+    expect(receipt).to.have.property('contractAddress')
+    assert.isNotEmpty(receipt.contractAddress)
+    expect(receipt).to.have.property('logs')
+    expect(receipt).to.have.property('status')
+    expect(receipt).to.have.property('logsBloom')
+  })
+
+  it(
+  'Should call the deploy function',
   async () => {
     const inputs = CompileMod.constructSolcInputs('Test.sol', contract)
-    const outputs = await CompileMod.compile(inputs)
+    const output = CompileMod.compile(inputs).contracts
+    const solcExtract = CompileMod.extractFromSolcOutput(output)
 
-    const gas = 2000000
-
-    const rawTx = DeployMod.makeRawTransaction(outputs.contracts, '0x', gas)
+    const receipt = await DeployMod.deploy(account, privateKey, solcExtract)
     
-    expect(rawTx).to.have.property('from')
-    expect(rawTx).to.have.property('data')
-    expect(rawTx).to.have.property('gas')
-
-    assert.strictEqual(rawTx.gas, gas)
-    assert.strictEqual(rawTx.data, contractByteCode)
-    assert.strictEqual('0x', rawTx.from)
+    // Returns 'signAndSendTransaction' under the hood 
+    // no need to assert the values again
+    assert(receipt)
   })
 })
