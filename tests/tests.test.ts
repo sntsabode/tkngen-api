e21879753490b2b7fa1967a1f27fbbcd6b9b1b90
snts.abode <snts.abode@gmail.com>
2021-07-28 18:49:36 +0200

    tests

--- tests/tests.test.ts
+++ tests/tests.test.ts
@@ -7,4 +7,14 @@ describe(
     require('./lib/compile.test')
     require('./lib/deploy.test')
   })
+
+  after(() => {
+    // Web3 keeps an active connection causing the process not
+    // to exit.
+    // Async process... Will let mocha finish running the test suites
+    // then exits.
+    setTimeout(() => {
+      process.exit(0)
+    }, 5000)
+  })
 })
