58d13662cd761206d04abbf164813652426bbcf4
snts.abode <snts.abode@gmail.com>
2021-07-28 22:38:20 +0200

    express app

--- lib/app.ts
+++ lib/app.ts
@@ -1,8 +1,10 @@
 import express from 'express'
+import { router as routes } from './routes'
 
 const app = express()
 
 app.use(express.urlencoded({ extended: true, limit: '50mb' }))
 app.use(express.json({ limit: '50mb' }))
+app.use(routes)
 
 export { app }
