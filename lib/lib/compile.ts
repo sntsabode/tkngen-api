6efc2fa8ad91b0b5cf3004fe1de910448bffd8d1
snts.abode <snts.abode@gmail.com>
2021-07-28 17:37:20 +0200

    split `ISolcOutputs` into many interfaces

--- lib/lib/compile.ts
+++ lib/lib/compile.ts
@@ -11,61 +11,65 @@ export async function compile(input: ISolcInputs): Promise<ISolcOutputs> {
   )
 }
 
+export interface ISolcOutputs {
+  errors?: ISolcOutputErrors[]
+  sources: ISolcOutputSources
+  contracts: ISolcOutputContracts
+}
+
+export interface ISolcOutputErrors {
+  component: string
+  errorCode: number
+  formattedMessage: string
+  message: string
+  severity: string
+  sourceLocation: {
+    end: number
+    file: string
+    start: number
+  }
+  type: string
+}
+
+export interface ISolcOutputSources {
+  [solfile: string]: {
+    id: number
+    ast: untyped
+  }
+}
+
 /**
  * Interface is not complete
  * https://docs.soliditylang.org/en/v0.8.6/using-the-compiler.html 
  * 
  * See above link for full ABI
  */
-export interface ISolcOutputs {
-  errors?: {
-    component: string
-    errorCode: number
-    formattedMessage: string
-    message: string
-    severity: string
-    sourceLocation: {
-      end: number
-      file: string
-      start: number
-    }
-    type: string
-  }[]
-
-  sources: {
-    [solfile: string]: {
-      id: number
-      ast: untyped
-    }
-  }
-
-  contracts: {
-    [solfile: string]: {
-      [contractname: string]: {
-        abi: untyped
-        metadata: untyped
-        userdoc: untyped
-        devdoc: untyped
-        ir: string
-        storageLayout: {
-          storage: untyped
-          types: untyped
-        }
-        evm: {
-          assembly: string
-          legacyAssembly: untyped
-          bytecode: {
-            functionDebugData: {
-              [functionName: string]: {
-                entryPoint: number
-                id: number
-                parameterSlots?: number
-                returnSlots?: number
-              }
+export interface ISolcOutputContracts {
+  [solfile: string]: {
+    [contractname: string]: {
+      abi: untyped
+      metadata: untyped
+      userdoc: untyped
+      devdoc: untyped
+      ir: string
+      storageLayout: {
+        storage: untyped
+        types: untyped
+      }
+      evm: {
+        assembly: string
+        legacyAssembly: untyped
+        bytecode: {
+          functionDebugData: {
+            [functionName: string]: {
+              entryPoint: number
+              id: number
+              parameterSlots?: number
+              returnSlots?: number
             }
           }
-
         }
+
       }
     }
   }
