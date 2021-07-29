377658c1971b449a992928a31afa5d030dd28815
snts.abode <snts.abode@gmail.com>
2021-07-29 10:15:41 +0200

    web3 instances

--- lib/web3.ts
+++ lib/web3.ts
@@ -7,10 +7,37 @@ dotenvConfig()
 if (!process.env.ETH_NODE_URL)
   throw panic('ETH_NODE_URL undefined')
 
-export const web3 = process.env.NODE_ENV === 'production'
-  ? new Web3(
-    new Web3.providers.WebsocketProvider(process.env.ETH_NODE_URL)
-  )
-  : new Web3(
+export type Web3Instance =
+  | 'MAINNET'
+  | 'MAINNET_FORK'
+  | 'KOVAN'
+  | 'BINANCESMARTCHAIN'
+  | 'BINANCESMARTCHAIN_TEST'
+
+/* eslint-disable */
+
+const Web3Instances = {
+  MAINNET: () => new Web3(
+    new Web3.providers.WebsocketProvider(process.env.ETH_NODE_URL!)
+  ),
+
+  MAINNET_FORK: () => new Web3(
     new Web3.providers.WebsocketProvider('http://localhost:7545')
+  ),
+
+  KOVAN: () => new Web3(
+    new Web3.providers.WebsocketProvider(process.env.ETH_NODE_URL_KOVAN!)
+  ),
+
+  BINANCESMARTCHAIN: () => new Web3(
+    new Web3.providers.WebsocketProvider(process.env.BNSC_NODE_URL!)
+  ),
+
+  BINANCESMARTCHAIN_TEST: () => new Web3(
+    new Web3.providers.WebsocketProvider(process.env.BNSC_TEST_NODE_URL!)
   )
+}
+
+/* eslint-enable */
+
+export const Web3Fac = (instance: Web3Instance): Web3 => Web3Instances[instance]()
