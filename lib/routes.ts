bb692e4a12a5df856914ca1103af2515754aaa17
snts.abode <snts.abode@gmail.com>
2021-08-03 19:40:39 +0200

    added validation to requests

--- lib/routes.ts
+++ lib/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import * as ERC20Controllers from './controllers/ERC20.controller'
 import * as BEP20Controllers from './controllers/BEP20.controller'
+import { RequestValidation } from './controllers/__req.validation__'
 
 const router = Router()
 
@@ -8,7 +9,16 @@ router.get('/ping', async (req, res) => {
   return res.status(200).send('ping')
 })
 
-router.post('/ERC-20/Standard', ERC20Controllers.StandardERC20Route)
-router.post('/BEP-20/Standard', BEP20Controllers.StandardBEP20Route)
+router.post(
+  '/ERC-20/Standard',
+  RequestValidation,
+  ERC20Controllers.StandardERC20Route
+)
+
+router.post(
+  '/BEP-20/Standard',
+  RequestValidation,
+  BEP20Controllers.StandardBEP20Route
+)
 
 export { router }
