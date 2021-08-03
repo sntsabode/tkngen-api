789cac2f8746fc6195e40fa22edd81841958d99e
snts.abode <snts.abode@gmail.com>
2021-08-03 19:40:26 +0200

    enabled cors

--- lib/app.ts
+++ lib/app.ts
@@ -1,10 +1,14 @@
 import express from 'express'
+import cors from 'cors'
 import { router as routes } from './routes'
+import { untyped } from './utils'
 
 const app = express()
 
+app.options('*', cors() as untyped)
 app.use(express.urlencoded({ extended: true, limit: '50mb' }))
 app.use(express.json({ limit: '50mb' }))
+app.use(cors())
 app.use(routes)
 
 export { app }
