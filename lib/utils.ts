0b68a589cb8e98579d27a66b097bd266601a9a7e
snts.abode <snts.abode@gmail.com>
2021-08-04 19:41:20 +0200

    added `convertToWei` & friends

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,30 @@
 // eslint-disable-next-line
 export type untyped = any
+import BN from 'bn.js'
+import Web3 from 'web3'
+import { Unit } from 'web3-utils'
+
+const Decimals = {
+  '18': 'ether',
+  '12': 'szabo',
+  '6': 'mwei'
+}
+
+export type SupportedDecimals =
+  | '18'
+  | '12'
+  | '8'
+  | '6'
+
+export function convertToWei(
+  val: string,
+  decimals: SupportedDecimals,
+): BN {
+  if (decimals === '8')
+    return new BN(val).mul(new BN(10 ** 8))
+    
+  return new BN(new Web3().utils.toWei(val, (Decimals[decimals] as Unit)))
+}
 
 export function panic<E>(...msg: E[]): never {
   console.error(...msg)
