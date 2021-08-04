906d5471ff9cc427567fbd964add7a629a8d190d
snts.abode <snts.abode@gmail.com>
2021-08-04 19:14:19 +0200

    added `MintableBurnableBEP20Route`

--- lib/controllers/BEP20.controller.ts
+++ lib/controllers/BEP20.controller.ts
@@ -3,8 +3,9 @@ import {
   Response as res
 } from 'express'
 import { validationResult } from 'express-validator'
+import { MintableBurnableBEP20 } from '../../__contracts__/BEP/MintableBurnableBEP20'
 import { StandardBEP20 } from '../../__contracts__/BEP/StandardBEP20'
-import { compile, constructSolcInputs } from '../lib/compile'
+import { compile, constructSolcInputs, ISolcInputs } from '../lib/compile'
 import { Web3Fac } from '../web3'
 import { IRequestBody } from './__req.body__'
 
@@ -21,6 +22,44 @@ export async function StandardBEP20Route(
     totalSupply, privateKey, network
   } = <IRequestBody>req.body
 
+  return Route({
+    network, tokenName, privateKey
+  }, constructSolcInputs(
+    tokenName, StandardBEP20(
+      '0.8.6', tokenName, tokenSymbol, 
+      tokenDecimals, totalSupply
+    )
+  ), res)
+}
+
+export async function MintableBurnableBEP20Route(
+  req: req, res: res
+): Promise<res> {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) return res.status(400).send(
+    { success: false, err: errors }
+  )
+
+  const {
+    tokenName, tokenDecimals, tokenSymbol,
+    totalSupply, privateKey, network
+  } = <IRequestBody>req.body
+
+  return Route({
+    network, tokenName, privateKey
+  }, constructSolcInputs(
+    tokenName, MintableBurnableBEP20(
+      '0.8.6', tokenName, tokenSymbol,
+      tokenDecimals, totalSupply
+    )
+  ), res)
+}
+
+async function Route(
+  { network, tokenName, privateKey }: Partial<IRequestBody>,
+  inputs: ISolcInputs,
+  res: res
+): Promise<res> {
   if (
     network !== 'BINANCESMARTCHAIN'
     && network !== 'BINANCESMARTCHAIN_TEST'
@@ -31,16 +70,13 @@ export async function StandardBEP20Route(
 
   try {
     const web3 = Web3Fac(network)
-    const outputs = compile(constructSolcInputs(
-      tokenName, StandardBEP20(
-        '0.8.6', tokenName, tokenSymbol, 
-        tokenDecimals, <unknown>totalSupply as string
-      )
-    ))
+    const outputs = compile(inputs)
 
-    const ABI = outputs.contracts[tokenName][tokenName].abi
-    const evmBytecode = outputs.contracts[tokenName][tokenName].evm.bytecode.object
-    const account = web3.eth.accounts.privateKeyToAccount(privateKey)
+    /* eslint-disable */
+    const ABI = outputs.contracts[tokenName!][tokenName!].abi
+    const evmBytecode = outputs.contracts[tokenName!][tokenName!].evm.bytecode.object
+    const account = web3.eth.accounts.privateKeyToAccount(privateKey!)
+    /* eslint-enable */
 
     return res.status(200).send({
       success: true,
@@ -51,7 +87,8 @@ export async function StandardBEP20Route(
           gas: 5000000,
           gasPrice: 18e9,
           data: evmBytecode
-        }, privateKey).then(signedTX => web3.eth.sendSignedTransaction(
+          // eslint-disable-next-line
+        }, privateKey!).then(signedTX => web3.eth.sendSignedTransaction(
           // eslint-disable-next-line
           signedTX.rawTransaction!
         ))),
