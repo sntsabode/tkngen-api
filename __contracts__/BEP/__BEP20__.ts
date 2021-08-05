f8ed1d93f7ed937708813387a694642974b9fb48
snts.abode <snts.abode@gmail.com>
2021-08-05 18:15:38 +0200

    BEP20 module

---
+++ __contracts__/BEP/__BEP20__.ts
@@ -0,0 +1,5 @@
+import { StandardBEP20 } from './StandardBEP20'
+import { MintableBurnableBEP20 } from './MintableBurnableBEP20'
+
+export { StandardBEP20 as Standard }
+export { MintableBurnableBEP20 as MintableBurnable }
