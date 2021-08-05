3a51b7ee14544a3629aff4f3b761340f9f40490b
snts.abode <snts.abode@gmail.com>
2021-08-05 18:17:27 +0200

    refactor

--- lib/controllers/BEP20.controller.ts
+++ lib/controllers/BEP20.controller.ts
@@ -2,111 +2,18 @@ import {
   Request as req,
   Response as res
 } from 'express'
-import { validationResult } from 'express-validator'
-import { MintableBurnableBEP20 } from '../../__contracts__/BEP/MintableBurnableBEP20'
-import { StandardBEP20 } from '../../__contracts__/BEP/StandardBEP20'
-import { compile, constructSolcInputs, ISolcInputs } from '../lib/compile'
-import { Web3Fac } from '../web3'
-import { IRequestBody } from './__req.body__'
-import { convertToWei, SupportedDecimals } from '../utils'
+import { RouteEntryPoint } from './__route__'
 
-export async function StandardBEP20Route(
+export const StandardBEP20Route = (
   req: req, res: res
-): Promise<res> {
-  const errors = validationResult(req)
-  if (!errors.isEmpty()) return res.status(400).send(
-    { success: false, err: errors }
-  )
+): Promise<res> => RouteEntryPoint(
+  req, res, 'BEP20', 'Standard',
+  'BINANCESMARTCHAIN', 'BINANCESMARTCHAIN_TEST'
+)
 
-  const {
-    tokenName, tokenDecimals, tokenSymbol,
-    totalSupply, privateKey, network
-  } = <IRequestBody>req.body
-
-  return Route({
-    network, tokenName, privateKey
-  }, constructSolcInputs(
-    tokenName, StandardBEP20(
-      '0.8.6', tokenName, tokenSymbol, 
-      tokenDecimals, convertToWei(totalSupply.toString(),
-        (tokenDecimals.toString() as SupportedDecimals)
-      ).toString()
-    )
-  ), res)
-}
-
-export async function MintableBurnableBEP20Route(
+export const MintableBurnableBEP20Route = (
   req: req, res: res
-): Promise<res> {
-  const errors = validationResult(req)
-  if (!errors.isEmpty()) return res.status(400).send(
-    { success: false, err: errors }
-  )
-
-  const {
-    tokenName, tokenDecimals, tokenSymbol,
-    totalSupply, privateKey, network
-  } = <IRequestBody>req.body
-
-  return Route({
-    network, tokenName, privateKey
-  }, constructSolcInputs(
-    tokenName, MintableBurnableBEP20(
-      '0.8.6', tokenName, tokenSymbol,
-      tokenDecimals, convertToWei(totalSupply.toString(),
-        (tokenDecimals.toString() as SupportedDecimals)
-      ).toString()
-    )
-  ), res)
-}
-
-async function Route(
-  { network, tokenName, privateKey }: Partial<IRequestBody>,
-  inputs: ISolcInputs,
-  res: res
-): Promise<res> {
-  if (
-    network !== 'BINANCESMARTCHAIN'
-    && network !== 'BINANCESMARTCHAIN_TEST'
-  ) return res.status(400).send({
-    success: false,
-    msg: 'You\'ve entered an unsupported network'
-  })
-
-  try {
-    const web3 = Web3Fac(network)
-    const outputs = compile(inputs)
-
-    /* eslint-disable */
-    const ABI = outputs.contracts[tokenName!][tokenName!].abi
-    const evmBytecode = outputs.contracts[tokenName!][tokenName!].evm.bytecode.object
-    const account = web3.eth.accounts.privateKeyToAccount(privateKey!)
-    /* eslint-enable */
-
-    return res.status(200).send({
-      success: true,
-      msg: 'Successfuly deployed your BEP20 Token!',
-      data: {
-        receipt: (await web3.eth.accounts.signTransaction({
-          from: account.address,
-          gas: 5000000,
-          gasPrice: 18e9,
-          data: evmBytecode
-          // eslint-disable-next-line
-        }, privateKey!).then(signedTX => web3.eth.sendSignedTransaction(
-          // eslint-disable-next-line
-          signedTX.rawTransaction!
-        ))),
-        solc: {
-          ABI, evmBytecode
-        }
-      }
-    })
-  } catch (e) {
-    console.error(e)
-    return res.status(500).send({
-      success: false,
-      msg: 'Failed to deploy your BEP20 contract. Internal Error.'
-    })
-  }
-}
+): Promise<res> => RouteEntryPoint(
+  req, res, 'BEP20', 'MintableBurnable',
+  'BINANCESMARTCHAIN', 'BINANCESMARTCHAIN_TEST'
+)
--- lib/controllers/ERC20.controller.ts
+++ lib/controllers/ERC20.controller.ts
@@ -2,70 +2,11 @@ import {
   Request as req,
   Response as res
 } from 'express'
-import { validationResult } from 'express-validator'
-import { StandardERC20 } from '../../__contracts__/ERC/StandardERC20'
-import { compile, constructSolcInputs } from '../lib/compile'
-import { deploy } from '../lib/deploy'
-import { Web3Fac } from '../web3'
-import { IRequestBody } from './__req.body__'
-import { convertToWei, SupportedDecimals } from '../utils'
+import { RouteEntryPoint } from './__route__'
 
-const AccountUnlockDuration = 10000
-
-export async function StandardERC20Route(
+export const StandardERC20Route = (
   req: req, res: res
-): Promise<res> {
-  const errors = validationResult(req)
-  if (!errors.isEmpty()) return res.status(400).send(
-    { success: false, err: errors }
-  )
-
-  const {
-    tokenName, tokenDecimals, tokenSymbol,
-    totalSupply, privateKey, network
-  } = <IRequestBody>req.body
-
-  const web3 = Web3Fac(network)
-  if (!web3) return res.status(400).send({
-    success: false,
-    msg: 'You\'ve entered an unsupported network'
-  })
-  
-  try {
-    const fromAccount = web3.eth.accounts.privateKeyToAccount(privateKey)
-    const outputs = compile(constructSolcInputs(
-      tokenName, StandardERC20('0.8.6', tokenName, tokenDecimals, convertToWei(
-        totalSupply.toString(), (tokenDecimals.toString() as SupportedDecimals)
-      ).toString())
-    ))
-
-    const ABI = outputs.contracts[tokenName][tokenName].abi
-    const evmBytecode = outputs.contracts[tokenName][tokenName].evm.bytecode.object
-
-    await web3.eth.personal.unlockAccount(
-      fromAccount.address,
-      fromAccount.privateKey,
-      AccountUnlockDuration
-    )
-
-    return res.status(200).send({
-      success: true,
-      msg: 'Successfuly deployed your ERC20 Token!',
-      data: {
-        receipt: (await deploy({
-          ABI,
-          metadata: '',
-          evmBytecode
-        }, web3, [tokenName, tokenSymbol], fromAccount.address)),
-        solc: { ABI, evmBytecode }
-      }
-    })
-  } catch (e) {
-    console.error(e)
-
-    return res.status(500).send({
-      success: false,
-      msg: 'Failed to deploy your ERC20 Token. Internal error'
-    })
-  }
-}
+): Promise<res> => RouteEntryPoint(
+  req, res, 'ERC20', 'Standard',
+  'MAINNET', 'KOVAN', 'MAINNET_FORK'
+)
