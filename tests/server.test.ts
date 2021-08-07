1f37570757cd2ee813d0140372c054db6c3eb633
snts.abode <snts.abode@gmail.com>
2021-08-07 18:12:32 +0200

    server test suite

--- tests/server.test.ts
+++ tests/server.test.ts
@@ -4,12 +4,9 @@ yarn run mocha -r ts-node/register tests/server.test.ts --timeout 9999999999
 import chaiHttp from 'chai-http'
 import chai, { expect } from 'chai'
 import { app } from '../lib/app'
-import { spawnGanache } from '../chain-fork'
 require('dotenv').config()
 
 chai.use(chaiHttp)
-
-const ETH_NODE_URL = process.env.ETH_NODE_URL!
 
 describe(
 'tkngen-api server test suite',
@@ -21,17 +18,6 @@ describe(
     expect(res).to.have.status(200)
   })
 
-  describe('ERC20 endpoints test suite', () => {
-    const ganacheInstanceETH = spawnGanache(['--fork', ETH_NODE_URL, '--port', 7545])
-    
-    before(async () => {
-      // waiting for ganache to boot
-      return new Promise((resolve) => setTimeout(() => resolve(), 5000))
-    })
-
-    it('', () => require('./ERC20.endpoints.test'))
-
-    after(() => ganacheInstanceETH.kill(0))
-  })
+  require('./ERC20.endpoints.test')
   require('./BEP20.endpoints.test')
 })
