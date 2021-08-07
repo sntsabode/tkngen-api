858b099a2916a1dd23eddc0c422278906d987617
snts.abode <snts.abode@gmail.com>
2021-08-07 07:05:18 +0200

    BEP20 token contracts

--- __contracts__/BEP/__BEP20__.ts
+++ __contracts__/BEP/__BEP20__.ts
@@ -1,9 +1,9 @@
 import { StandardBEP20 } from './StandardBEP20'
+import { BurnableBEP20 } from './BurnableBEP20'
+import { MintableBEP20 } from './MintableBEP20'
 import { MintableBurnableBEP20 } from './MintableBurnableBEP20'
 
 export { StandardBEP20 as Standard }
-// Replace with Burnable BEP20 contract
-export { StandardBEP20 as Burnable }
-// Replace with Mintable BEP20 contract
-export { StandardBEP20 as Mintable }
+export { BurnableBEP20 as Burnable }
+export { MintableBEP20 as Mintable }
 export { MintableBurnableBEP20 as MintableBurnable }
