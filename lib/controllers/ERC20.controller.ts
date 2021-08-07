d6491ed4a8cf1564f317e6fd3b5d802157158084
snts.abode <snts.abode@gmail.com>
2021-08-07 04:59:43 +0200

    added `MintableERC20Route` & `BurnableERC20Route`

--- lib/controllers/ERC20.controller.ts
+++ lib/controllers/ERC20.controller.ts
@@ -10,3 +10,23 @@ export const StandardERC20Route = (
   req, res, 'ERC20', 'Standard',
   'MAINNET', 'KOVAN', 'MAINNET_FORK'
 )
+
+export const MintableERC20Route = (
+  req: req, res: res
+): Promise<res> => RouteEntryPoint(
+  req, res, 'ERC20', 'Mintable', 'MAINNET', 'KOVAN', 'MAINNET_FORK',
+  (account, req, web3) => web3.eth.abi.encodeParameters(
+    ['string memory', 'string memory', 'uint8', 'uint256'],
+    [req.tokenName, req.tokenSymbol, req.tokenDecimals, req.totalSupply]
+  ).slice(2)
+)
+
+export const BurnableERC20Route = (
+  req: req, res: res
+): Promise<res> => RouteEntryPoint(
+  req, res, 'BEP20', 'Burnable', 'MAINNET', 'KOVAN', 'MAINNET_FORK',
+  (account, req, web3) => web3.eth.abi.encodeParameters(
+    ['string memory', 'string memory', 'uint8', 'uint256'],
+    [req.tokenName, req.tokenSymbol, req.tokenDecimals, req.totalSupply]
+  ).slice(2)
+)
