7646afd47b7e4c5c40a04f8996b5134130354f89
snts.abode <snts.abode@gmail.com>
2021-08-07 09:36:43 +0200

    boot ganache before tests

--- tests/__contracts__/ERC.test.ts
+++ tests/__contracts__/ERC.test.ts
@@ -1,8 +1,27 @@
+/*
+yarn run mocha -r ts-node/register tests/__contracts__/ERC.test.ts --timeout 99999
+*/
+import { spawnGanache } from '../../chain-fork'
+require('dotenv').config()
+
+const ETH_NODE_URL = process.env.ETH_NODE_URL!
+
+const ganacheInstance = spawnGanache(['--fork', ETH_NODE_URL, '--port', 7545])
+
 describe(
 'ERC Token test suite',
 () => {
-  require('./ERC/StandardERC20.test')
-  require('./ERC/BurnableERC20.test')
-  require('./ERC/MintableERC20.test')
-  require('./ERC/MintableBurnableERC20.test')
+  before(async () => {
+    // waiting for ganache to boot
+    return new Promise((resolve) => setTimeout(() => resolve(), 5000))
+  })
+
+  it('', () => {
+    require('./ERC/StandardERC20.test')
+    require('./ERC/BurnableERC20.test')
+    require('./ERC/MintableERC20.test')
+    require('./ERC/MintableBurnableERC20.test')
+  })
+
+  after(() => ganacheInstance.kill(0))
 })
--- tests/server.test.ts
+++ tests/server.test.ts
@@ -4,9 +4,12 @@ yarn run mocha -r ts-node/register tests/server.test.ts --timeout 9999999999
 import chaiHttp from 'chai-http'
 import chai, { expect } from 'chai'
 import { app } from '../lib/app'
+import { spawnGanache } from '../chain-fork'
 require('dotenv').config()
 
 chai.use(chaiHttp)
+
+const ETH_NODE_URL = process.env.ETH_NODE_URL!
 
 describe(
 'tkngen-api server test suite',
@@ -18,6 +21,17 @@ describe(
     expect(res).to.have.status(200)
   })
 
-  require('./ERC20.endpoints.test')
+  describe('ERC20 endpoints test suite', () => {
+    const ganacheInstanceETH = spawnGanache(['--fork', ETH_NODE_URL, '--port', 7545])
+    
+    before(async () => {
+      // waiting for ganache to boot
+      return new Promise((resolve) => setTimeout(() => resolve(), 5000))
+    })
+
+    it('', () => require('./ERC20.endpoints.test'))
+
+    after(() => ganacheInstanceETH.kill(0))
+  })
   require('./BEP20.endpoints.test')
 })
