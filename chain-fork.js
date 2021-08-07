ffcab2f7bb97672c6b189dd9594730f5888cf9d1
snts.abode <snts.abode@gmail.com>
2021-08-07 08:46:36 +0200

    chain fork script

--- chain-fork.js
+++ chain-fork.js
@@ -1,89 +1,50 @@
 /* eslint-disable */
-const ganache = require('ganache-cli')
-const Web3 = require('web3')
-const { writeFileSync, readFileSync } = require('fs')
-const { resolve } = require('path')
+
+const { spawn } = require('child_process')
+const yargs = require('yargs')
 require('dotenv').config()
 
-if (!process.env.ETH_NODE_URL) throw new Error(
-  'ETH_NODE_URL undefined.'
-)
+const argv = yargs
+  .option('ETH', {
+    alias: 'e',
+    describe: 'Run Ethereum Mainnet fork',
+    default: false,
+    type: 'boolean'
+  })
+  .option('BSC', {
+    alias: 'b',
+    describe: 'Run BinanceSmartChain Mainnet fork',
+    default: false,
+    type: 'boolean'
+  }).argv
 
-const formatIntoMB = (param) => {
-  return Math.round(param / 1024 / 1024 * 100) / 100
-}
+const BSC_NODE_URL = process.env.BNSC_NODE_URL
+const ETH_NODE_URL = process.env.ETH_NODE_URL
 
-/*setInterval(() => {
-  console.log(`\nMemory\n${line}`)
-  for (const [key, value] of Object.entries(process.memoryUsage())) {
-    console.log(`\n${key}: ${formatIntoMB(value)} MB`)
-  }
-  console.log('\n')
-}, 300000)*/
+const spawnGanache = (args) => new Promise((resolve, reject) => {
+  const dir = process.cwd()
+  const childProcess = spawn('node', ['./ganache.js', ...args], {
+    stdio: 'inherit',
+    cwd: dir
+  })
 
-const server = ganache.server({
-  port: 7545,
-  default_balance_ether: 100,
-  fork: new Web3.providers.WebsocketProvider(process.env.ETH_NODE_URL),
-  ws: true,
-  debug: true,
-  vmErrorsOnRPCResponse: true,
-  verbose: true,
-  logger: console
+  childProcess.on('error', (err) => reject(err))
+  childProcess.on('close', (code, signal) => resolve({ code, signal }))
 })
 
-const PORT = 7545
-const line = '===================='
-
-server.listen(PORT, onboot)
-
-const accountsFile = (
-  privateKey,
-  account
-) => `
-module.exports = {
-  privateKey: '${privateKey}',
-  account: '${account}'
-}
-`.trim()
-
-function onboot(err, blockchain) {
-  if (err) {
-    console.error(`Error booting ganache: ${err}`)
-    return process.exit(1)
-  }
-
-  let count = 1
-  console.log(`\nAccounts\n${line}\n`)
-
-  const path = resolve('tests/__accounts__.js')
-
-  const [accountAddress, testAccount] = Object.entries(blockchain.accounts)[0]
-  const accounts = readFileSync(path).toString()
-  console.log(accounts)
-
-  writeFileSync(path, accountsFile(
-    '0x' + testAccount.secretKey.toString('hex'),
-    accountAddress
-  ))
-
-  for (let [key, value] of Object.entries(blockchain.accounts)) {
-    const address = '0x' + value.secretKey.toString('hex')
-
-    console.log(`${count} Account: ${key} ::: Private Key: ${address}`)
-    count++
-  }
-
-  console.log(`\n${line}\n`)
-
-  console.log(`\nForked Chain\n${line}\n`)
-  console.log(`Block:       `, blockchain.blockchain.forkBlockNumber)
-  console.log(`Network ID:  `, blockchain.net_version)
-  console.log(`Time:        `, (blockchain.blockchain.startTime || new Date()).toString())  
-
-  console.log(`\n${line}\n`)
-  console.log(`Ganache booted successfuly: ${('http://localhost:' + PORT)}`)
-  console.log(`\n${line}\n`)
+/**
+ * @param {boolean} BSC 
+ * @param {boolean} ETH 
+ */
+const bootInstance = async (BSC, ETH) => {
+  if (BSC) return spawnGanache(['--fork', BSC_NODE_URL, '--port', '7545'])
+  else if (ETH) return spawnGanache(['--fork', ETH_NODE_URL, '--port', '8545'])
 }
 
-/* eslint-enable */
+;(async () => {
+  await bootInstance(argv.BSC, argv.ETH)
+})().then(() => console.log('Finished'))
+
+module.export = {
+  bootInstances, spawnGanache
+}
