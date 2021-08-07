ab69a6b4b0acbfb8d2fa0e21a0a5a4217f432519
snts.abode <snts.abode@gmail.com>
2021-08-07 07:04:08 +0200

    ERC20 token contracts

--- __contracts__/ERC/__ERC20__.ts
+++ __contracts__/ERC/__ERC20__.ts
@@ -1,9 +1,9 @@
 import { StandardERC20 } from './StandardERC20'
 import { BurnableERC20 } from './BurnableERC20'
 import { MintableERC20 } from './MintableERC20'
+import { MintableBurnableERC20 } from './MintableBurnableERC20'
 
 export { StandardERC20 as Standard }
 export { BurnableERC20 as Burnable }
 export { MintableERC20 as Mintable }
-// Replace with MintableBurnable ERC20 contract
-export { StandardERC20 as MintableBurnable}
+export { MintableBurnableERC20 as MintableBurnable }
