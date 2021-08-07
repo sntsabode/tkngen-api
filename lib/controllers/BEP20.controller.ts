b6b7b72d5f94d9ed2c5c50e1e71397acabebdba9
snts.abode <snts.abode@gmail.com>
2021-08-07 07:12:05 +0200

    `BEP20` controller

--- lib/controllers/BEP20.controller.ts
+++ lib/controllers/BEP20.controller.ts
@@ -2,18 +2,29 @@ import {
   Request as req,
   Response as res
 } from 'express'
-import { RouteEntryPoint } from './__route__'
+import { RouteEntryPoint, RouteEntryPoint_ } from './__route__'
 
 export const StandardBEP20Route = (
   req: req, res: res
-): Promise<res> => RouteEntryPoint(
+): Promise<res> => RouteEntryPoint_(
   req, res, 'BEP20', 'Standard',
   ['BINANCESMARTCHAIN', 'BINANCESMARTCHAIN_TEST']
 )
 
+export const MintableBEP20Route = (
+  req: req, res: res
+): Promise<res> => RouteEntryPoint('BEP20', 'Mintable', [
+  'BINANCESMARTCHAIN', 'BINANCESMARTCHAIN_TEST'
+])(req, res)
+
+export const BurnableBEP20Route = (
+  req: req, res: res
+): Promise<res> => RouteEntryPoint('BEP20', 'Burnable', [
+  'BINANCESMARTCHAIN', 'BINANCESMARTCHAIN_TEST'
+])(req, res)
+
 export const MintableBurnableBEP20Route = (
   req: req, res: res
-): Promise<res> => RouteEntryPoint(
-  req, res, 'BEP20', 'MintableBurnable',
-  ['BINANCESMARTCHAIN', 'BINANCESMARTCHAIN_TEST']
-)
+): Promise<res> => RouteEntryPoint('BEP20', 'MintableBurnable', [
+  'BINANCESMARTCHAIN', 'BINANCESMARTCHAIN_TEST'
+])(req, res)
