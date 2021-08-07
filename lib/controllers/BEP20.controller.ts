d590dd953fa61bf6fd9bd07aada4c35eefd57e4e
snts.abode <snts.abode@gmail.com>
2021-08-07 19:34:36 +0200

    controllers

--- lib/controllers/BEP20.controller.ts
+++ lib/controllers/BEP20.controller.ts
@@ -3,28 +3,28 @@ import {
   Response as res
 } from 'express'
 import { RouteEntryPoint, RouteEntryPoint_ } from './__route__'
+import { Networks } from './__networks__'
 
 export const StandardBEP20Route = (
   req: req, res: res
 ): Promise<res> => RouteEntryPoint_(
-  req, res, 'BEP20', 'Standard',
-  ['BINANCESMARTCHAIN', 'BINANCESMARTCHAIN_TEST']
+  req, res, 'BEP20', 'Standard', Networks.BEP20
 )
 
 export const MintableBEP20Route = (
   req: req, res: res
-): Promise<res> => RouteEntryPoint('BEP20', 'Mintable', [
-  'BINANCESMARTCHAIN', 'BINANCESMARTCHAIN_TEST'
-])(req, res)
+): Promise<res> => RouteEntryPoint(
+  'BEP20', 'Mintable', Networks.BEP20
+)(req, res)
 
 export const BurnableBEP20Route = (
   req: req, res: res
-): Promise<res> => RouteEntryPoint('BEP20', 'Burnable', [
-  'BINANCESMARTCHAIN', 'BINANCESMARTCHAIN_TEST'
-])(req, res)
+): Promise<res> => RouteEntryPoint(
+  'BEP20', 'Burnable', Networks.BEP20
+)(req, res)
 
 export const MintableBurnableBEP20Route = (
   req: req, res: res
-): Promise<res> => RouteEntryPoint('BEP20', 'MintableBurnable', [
-  'BINANCESMARTCHAIN', 'BINANCESMARTCHAIN_TEST'
-])(req, res)
+): Promise<res> => RouteEntryPoint(
+  'BEP20', 'MintableBurnable', Networks.BEP20
+)(req, res)
--- lib/controllers/ERC20.controller.ts
+++ lib/controllers/ERC20.controller.ts
@@ -2,29 +2,29 @@ import {
   Request as req,
   Response as res
 } from 'express'
+import { Networks } from './__networks__'
 import { RouteEntryPoint, RouteEntryPoint_ } from './__route__'
 
 export const StandardERC20Route = (
   req: req, res: res
 ): Promise<res> => RouteEntryPoint_(
-  req, res, 'ERC20', 'Standard',
-  ['MAINNET', 'KOVAN', 'MAINNET_FORK']
+  req, res, 'ERC20', 'Standard', Networks.ERC20
 )
 
 export const MintableERC20Route = (
   req: req, res: res
-): Promise<res> => RouteEntryPoint('ERC20', 'Mintable', [
-  'MAINNET', 'MAINNET_FORK', 'KOVAN'
-])(req, res)
+): Promise<res> => RouteEntryPoint(
+  'ERC20', 'Mintable', Networks.ERC20
+)(req, res)
 
 export const BurnableERC20Route = (
   req: req, res: res
-): Promise<res> => RouteEntryPoint('ERC20', 'Burnable', [
-  'MAINNET', 'MAINNET_FORK', 'KOVAN'
-])(req, res)
+): Promise<res> => RouteEntryPoint(
+  'ERC20', 'Burnable', Networks.ERC20
+)(req, res)
 
 export const MintableBurnableERC20Route = (
   req: req, res: res
-): Promise<res> => RouteEntryPoint('ERC20', 'MintableBurnable', [
-  'MAINNET', 'MAINNET_FORK', 'KOVAN'
-])(req, res)
+): Promise<res> => RouteEntryPoint(
+  'ERC20', 'MintableBurnable', Networks.ERC20
+)(req, res)
