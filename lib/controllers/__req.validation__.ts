da312ccb27568034824301adfdb5b7412ce612ea
snts.abode <snts.abode@gmail.com>
2021-08-08 23:27:54 +0200

    request validation

--- lib/controllers/__req.validation__.ts
+++ lib/controllers/__req.validation__.ts
@@ -5,30 +5,38 @@ import {
 function NoSpecialCharsValidation(
   input: string, meta: Meta
 ) {
-  if (/[^a-zA-Z0-9_]/.test(input)) {
+  if (/[^a-zA-Z0-9_]/.test(input))
     throw new Error('Found special characters: ' + meta.path)
-  }
 
+  return true
+}
+
+function AssertPrivateKeyLength(
+  input: string, meta: Meta
+) {
+  if (input.length !== 66)
+    throw new Error('Private Key has to be 66 chars long: ' + meta.path)
+  
   return true
 }
 
 export const RequestValidation = [
   validate('tokenName', 'Invalid token name')
     .isString()
-    .custom(NoSpecialCharsValidation)
     .escape()
-    .trim(),
+    .trim()
+    .custom(NoSpecialCharsValidation),
 
   validate('tokenDecimals', 'Invalid token decimals')
-    .isNumeric()  
+    .isNumeric()
     .escape()
     .trim(),
 
   validate('tokenSymbol', 'Invalid token symbol')
     .isString()
-    .custom(NoSpecialCharsValidation)
     .escape()
-    .trim(),
+    .trim()
+    .custom(NoSpecialCharsValidation),
 
   validate('totalSupply', 'Invalid total supply')
     .isNumeric()
@@ -38,7 +46,8 @@ export const RequestValidation = [
   validate('privateKey', 'Invalid private key')
     .isString()
     .escape()
-    .trim(),
+    .trim()
+    .custom(AssertPrivateKeyLength),
 
   validate('network', 'Invalid network')
     .isString()
