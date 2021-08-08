c2096fcd1cb17ea8ab5bd7d69f4d6cf3c7ea9d73
snts.abode <snts.abode@gmail.com>
2021-08-08 09:42:39 +0200

    StandardBEP20 token contract tests

--- tests/__contracts__/BEP/StandardBEP20.test.ts
+++ tests/__contracts__/BEP/StandardBEP20.test.ts
@@ -4,15 +4,7 @@ yarn run mocha -r ts-node/register tests/__contracts__/BEP/StandardBEP20.test.ts
 import { compile } from '../../../lib/lib/compile'
 import { StandardBEP20 } from '../../../__contracts__/BEP/StandardBEP20'
 import { constructSolcInputs } from '../../../lib/lib/compile'
-import { assert, /* expect */ } from 'chai'
-//import { Web3Fac } from '../../../lib/web3'
-import { config } from 'dotenv'
-config()
-
-//const web3 = Web3Fac('BINANCESMARTCHAIN_TEST')
-
-//const address = process.env.BNC_TEST_ACC!
-//const privateKey = process.env.BNC_TEST_PVTK!
+import { assert } from 'chai'
 
 describe('StandardBEP20 contract test suite', () => {
   it('Should compile the StandardBEP20 contract', done => {
@@ -27,47 +19,4 @@ describe('StandardBEP20 contract test suite', () => {
   
     done()
   })
-
-  /*it('Should compile and deploy the StandardBEP20 contract', async () => {
-    const TokenName = 'TestToken'
-    const TokenSym = 'BEP'
-    const TokenDecimals = 8
-    const TotalSupply = (1000 * 10**TokenDecimals)
-
-    const inputs = constructSolcInputs(TokenName, StandardBEP20(
-      '0.8.6', TokenName, TokenSym, TokenDecimals, TotalSupply.toString()
-    ))
-
-    const outputs = compile(inputs)
-
-    const ABI = outputs.contracts[TokenName][TokenName].abi
-    const evmBytecode = outputs.contracts[TokenName][TokenName].evm.bytecode.object
-
-    const account = web3.eth.accounts.privateKeyToAccount(privateKey)
-    
-    const tx = {
-      from: account.address,
-      gas: 5000000,
-      gasPrice: 18e9,
-      data: evmBytecode
-    }
-
-    const signedTX = await web3.eth.accounts.signTransaction(tx, privateKey)
-    const receipt = await web3.eth.sendSignedTransaction(signedTX.rawTransaction!)
-
-    expect(receipt).to.have.property('contractAddress')
-    assert.isNotEmpty(receipt.contractAddress)
-
-    const testBEP20TokenInstance = new web3.eth.Contract(ABI, receipt.contractAddress)
-
-    const [name, sym, decimals] = await Promise.all([
-      testBEP20TokenInstance.methods.name().call(),
-      testBEP20TokenInstance.methods.symbol().call(),
-      testBEP20TokenInstance.methods.decimals().call()
-    ])
-
-    assert.strictEqual(name, TokenName)
-    assert.strictEqual(sym, TokenSym)
-    assert.strictEqual(decimals, TokenDecimals.toString())
-  })*/
 })
