6e89e6bfc80701580e1e80c27a13f93ca2623c48
snts.abode <snts.abode@gmail.com>
2021-08-08 20:17:57 +0200

    test bash script

--- .test.sh
+++ .test.sh
@@ -1,15 +1,68 @@
 #!/bin/bash
 
-BootGanacheAndRunTests () {
+if [ $# -gt 1 ]
+then
+  echo "Too many arguments."
+  echo "Enter (--fork) or (--no_fork)"
+  echo
+
+  exit 1
+fi
+
+if [ $# -eq 0 ]
+then
+  echo "No arguments passed in running Ganache without forks."
+  echo
+
   for (( ; ; ))
   do
-    echo "Running Tests"
+    echo "Running tests."
     echo
- 
+
     (yarn run chain) & (sleep 10s && yarn run tests)
 
     sleep 10000
   done
-}
+fi
 
-BootGanacheAndRunTests
+if [ $1 != "--fork" ] && [ $1 != "--no_fork" ]
+then
+  echo "Invalid argument."
+  echo "Please pass in (--fork) or (--no_fork)."
+  echo "You passed in" $1 "."
+  echo
+
+  exit 1
+fi
+
+if [ $1 == "--fork" ]
+then
+  echo "Running Ganache with forks (--fork parameter passed in)"
+  echo
+
+  for (( ; ; ))
+  do
+    echo "Running Tests"
+    echo
+
+    (yarn run chain:fork) & (sleep 10s && yarn run tests)
+
+    sleep 10000
+  done
+fi
+
+if [ $1 == "--no_fork" ]
+then
+  echo "Running Ganache without forks (--no_fork parameter passed in)"
+  echo
+
+  for (( ; ; ))
+  do
+    echo "Running Tests"
+    echo
+
+    (yarn run chain) & (sleep 10s && yarn run tests)
+
+    sleep 10000
+  done
+fi
