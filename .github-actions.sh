c33a347bd1098a906023a1b244c7680b6eb1aed4
snts.abode <snts.abode@gmail.com>
2021-08-08 20:27:30 +0200

    github actions runner

--- .github-actions.sh
+++ .github-actions.sh
@@ -2,4 +2,4 @@
 
 # leaks ganache instance. Don't run this on your computer
 # meant to be run by Github's action runner
-yarn run chain:no_fork & (sleep 10s && yarn run testsuite)
+yarn run chain & (sleep 10s && yarn run testsuite)
