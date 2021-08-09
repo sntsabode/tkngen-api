666bd47f52bdcffb1754f730872d36cf68b5e7d0
snts.abode <snts.abode@gmail.com>
2021-08-09 02:51:00 +0200

    web3 instance

--- lib/web3.ts
+++ lib/web3.ts
@@ -29,9 +29,9 @@ const NODE_URLs: {
     MAINNET: assert_env_var(process.env.ETH_NODE_URL),
     MAINNET_FORK: 'http://localhost:7545',
     KOVAN: assert_env_var(process.env.ETH_NODE_URL_KOVAN),
-    BINANCESMARTCHAIN: assert_env_var(process.env.BNSC_NODE_URL),
+    BINANCESMARTCHAIN: assert_env_var(process.env.BSC_NODE_URL),
     BINANCESMARTCHAIN_FORK: 'http://localhost:8545',
-    BINANCESMARTCHAIN_TEST: assert_env_var(process.env.BNSC_TEST_NODE_URL)
+    BINANCESMARTCHAIN_TEST: assert_env_var(process.env.BSC_TEST_NODE_URL)
   })
 })()
 
