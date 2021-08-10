a3b9ffd05300a4cd89ee860a77bdef518211d41e
snts.abode <snts.abode@gmail.com>
2021-08-10 22:51:54 +0200

    ganache instances bash scripts

--- .ganache/.ganache_bsc.sh
+++ .ganache/.ganache_bsc.sh
@@ -1,11 +1,40 @@
 #!/bin/bash
 
-for (( ; ; ))
-do
-  echo "Booting Ganache instances"
-  echo
+if [ $# -eq 0 ]
+then
+  for (( ; ; ))
+  do
+    echo "Booting Ganache instances"
+    echo
 
-  node ./.ganache/.ganache.js -bqw
+    node ./.ganache/.ganache.js -bqw --no_fork
 
-  sleep 10000
-done
+    sleep 10000
+  done
+fi
+
+if [ $1 == "--fork" ]
+then
+  for (( ; ; ))
+  do
+    echo "Booting Ganache instances"
+    echo
+
+    node ./.ganache/.ganache.js -bqw
+
+    sleep 10000
+  done
+fi
+
+if [ $1 == "--no_fork" ]
+then
+  for (( ; ; ))
+  do
+    echo "Booting Ganache instances"
+    echo
+
+    node ./.ganache/.ganache.js -bqw --no_fork
+
+    sleep 10000
+  done
+fi
